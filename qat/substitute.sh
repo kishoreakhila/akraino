87fcd2fa11e398ed69ebd3b635b6e588c95891ca
kishoreakhila <akhilakishore@gmail.com>
2019-11-14 16:45:34 -0800

    Update substitute.sh

--- qat/substitute.sh
+++ qat/substitute.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for file in $(find /root/qat_driver/*.conf -type f -name "*dev*.conf"); do
-       device_id=$( echo $file | cut -d '_' -f 3 | tr -cd '[[:digit:]]')
+for file in $(find /etc/*.conf -type f -name "c6xxvf_dev*.conf"); do
+       device_id=$( echo $file | cut -d '_' -f 2 | tr -cd '[[:digit:]]')
        echo $device_id
-       sudo sed -i "s/\[SSL\]/\[SSL${device_id}\]/g" /root/qat_driver/a6xxvf_dev${device_id}.conf
+       cat /etc/c6xxvf_dev${device_id}.conf
+       sudo sed -i "s/\[SSL\]/\[SSL${device_id}\]/g" /etc/c6xxvf_dev${device_id}.conf
 done
