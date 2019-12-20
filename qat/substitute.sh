78c1f2dc6cc950fb3a8abdbbebd7767b000e9084
kishoreakhila <akhilakishore@gmail.com>
2019-12-20 14:25:35 -0800

    update to include different kinds of config file

--- qat/substitute.sh
+++ qat/substitute.sh
@@ -6,3 +6,10 @@ for file in $(find /etc/*.conf -type f -name "c6xxvf_dev*.conf"); do
        cat /etc/c6xxvf_dev${device_id}.conf
        sudo sed -i "s/\[SSL\]/\[SSL${device_id}\]/g" /etc/c6xxvf_dev${device_id}.conf
 done
+
+for file in $(find /etc/*.conf -type f -name "c6xx_dev*.conf"); do
+       dev_id=$( echo $file | cut -d '_' -f 2 | tr -cd '[[:digit:]]')
+       echo $dev_id
+       cat /etc/c6xx_dev${dev_id}.conf
+       sudo sed -i "s/\[SSL\]/\[SSL${dev_id}\]/g" /etc/c6xx_dev${dev_id}.conf
+done
