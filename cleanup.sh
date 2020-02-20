a02e076120046b2bd6ae3032a984f1b8e4f9b7fa
kishoreakhila <akhilakishore@gmail.com>
2020-02-20 12:50:30 -0800

    Update cleanup.sh
    Updating it with if/else ladder for better readability.

--- cleanup.sh
+++ cleanup.sh
@@ -18,23 +18,30 @@ if [ "$1" == "--reset" ]; then
     docker rm -vf $(docker ps -a -q) 2>/dev/null || \
     echo "No more containers to remove."
 elif [ "$1" == "--purge" ]; then
+    if [ -z "$2" ]; then
+        echo "Cannot purge. Please provide image name to purge."
+        exit 0
+    fi
     matchExp=$2
-    echo "Match exp here: $matchExp ------>"
     # Attempt to remove running containers that are using the images we're trying to purge first.
-    docker rm -vf $(docker ps -a --format "{{.Image}} {{.Names}} {{.ID}}" | grep "$matchExp" | awk -v matchExp="$2" '{print $2}') 2>/dev/null
-
-    #docker rm -vf $(docker ps -a --format "{{.Image}} {{.Names}} {{.ID}}" | \
-    #awk -v matchExp="$2" '$0 ~ matchExp {print $2}') 2>/dev/null
-    #echo "Here:------------> grep match"
-    #docker images --format "{{.Repository}}: {{.ID}}" | grep "$matchExp"
-    echo "Here:------------> grep and awk"
-    # Remove all images matching arg given after "--purge"`
-    returnVal=$?
-    #echo "Return value: $returnVal ----->"
-    if [ $returnVal -ne 0 ]; then
-        echo "No running containers using the \"$2\" image, continuing purge."
+    if [[ $(docker ps --filter "name=$matchExp") ]]; then
+        echo "Removing running containers using the \"$2\" container image"
+        docker rm -vf $(docker ps -a --format "{{.Image}} {{.Names}}" | \
+        awk '$0 ~ matchExp {print $2}') 2>/dev/null
     else
-        docker images --format "{{.Repository}}:{{.Tag}}" | grep "$matchExp" | awk -v matchExp="$2" '$0 ~ matchExp {print $1}' | xargs docker rmi 2>/dev/null
+        echo "No running containers using the \"$2\" container image"
+    fi
+    echo "Continue to purge container images..."
+    # Remove all images matching arg given after "--purge"`
+    docker images -q --format "{{.Repository}}:{{.Tag}}" | grep "$matchExp"
+    returnVal=$?
+    if [ $returnVal -ne 0 ]; then
+        echo "No \"$2\" container image found."
+        exit 0
+    else
+        echo "Removing all the \"$2\" container images."
+        docker images --format "{{.Repository}}:{{.Tag}}" | grep "$matchExp" |\
+        awk '$0 ~ matchExp {print $1}' | xargs docker rmi 2>/dev/null
         exit 0
     fi
 else
@@ -46,7 +53,8 @@ fi
 if [ "$1" == "--nuclear" ]; then
     docker rm -vf $(docker ps -a -q) 2>/dev/null || \
     echo "No more containers to remove."
-    docker rmi $(docker images -q) 2>/dev/null || echo "No more images to remove."
+    docker rmi $(docker images -q) 2>/dev/null || \
+    echo "No more images to remove."
     echo "Preparing to uninstall docker ....."
     dpkg -l | grep Docker | awk '{print $2}' > /tmp/docker-list.txt
     while read list; do
